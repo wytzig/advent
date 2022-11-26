c04383df3f78fc30ef9ce827efc2966db8f2f5c1
wytzig <luukweiss@gmail.com>
2022-11-26 23:26:36 +0100

    call numbers and check for bingo working for example

--- src/main/java/com/wytzig/lw/nl/advent/twentytwentyone/bingo/BingoCard.java
+++ src/main/java/com/wytzig/lw/nl/advent/twentytwentyone/bingo/BingoCard.java
@@ -1,9 +1,13 @@
 package com.wytzig.lw.nl.advent.twentytwentyone.bingo;
 
+import org.bouncycastle.asn1.esf.SPUserNotice;
+
 public class BingoCard {
     private final String name;
 
-    private int[][] initalBingoCard = new int[5][5];
+    int[][] initalBingoCard = new int[5][5];
+
+    boolean[][] markedCard = new boolean[5][5];
 
     public BingoCard(String name) {
         this.name = name;
@@ -11,6 +15,75 @@ public class BingoCard {
 
     public int getNumberForRowAndColumn(int row, int column) {
         return initalBingoCard[row][column];
+    }
+
+    //call with a given next number
+    //if the number is on the card, the number will be 'marked'.
+    //if a full column or a full row is marked, then we have bingo
+    public boolean callNumberAndCheckForBingo(int calledNumber) {
+        System.out.println("-------------");
+        System.out.println("-------------");
+        System.out.println("call number: "+ calledNumber);
+        // check number and fill the marked card
+        isPresentInCard(calledNumber);
+
+        // check row's for bingo
+        return checkForBingo();
+    }
+
+    private boolean checkForBingo() {
+        int rowSucces = 0;
+        int columnSucces = 0;
+        for(int i = 0; i < 5; i++) {
+            for (int j = 0; j <5; j++) {
+                // check every row
+                if(markedCard[i][j] == true) rowSucces++;
+                if(rowSucces == 5) {
+                    bingoPrint(rowSucces, columnSucces);
+                    return true;
+                }
+            }
+            // next column so reset row succes
+            rowSucces = 0;
+
+            if(markedCard[i][0] == true) columnSucces++;
+            if(columnSucces == 5) {
+                bingoPrint(rowSucces, columnSucces);
+                return true;
+            }
+        }
+        // assert that row and column successes cannot be >5
+        assert columnSucces <= 5;
+        return false;
+    }
+
+    private void bingoPrint(int rowSucces, int columnSucces) {
+        System.out.println("rowsucces: " + rowSucces + " and columnSucces: " + columnSucces);
+        System.out.println("WE HAVE A BINGO!!!");
+        System.out.println("Card: " + this.name);
+
+    }
+
+
+    /**
+     * Also sets a true flag in the markedBingoCard list on the pos (x,y) that if found
+     * the number
+     * @param calledNumber the number drawn by the bingoMeister
+     * @return true if the number exists on the bingo card, false otherwise
+     */
+    private boolean isPresentInCard(int calledNumber) {
+        for(int i = 0; i < 5; i++) { // every row
+            for (int j = 0; j < 5; j++) { // every column
+                if(initalBingoCard[i][j] == calledNumber) {
+                    System.out.println("\ncalled number: " + calledNumber + " was present on" + this.name + " on pos: " + (i + 1) + ", " + (j + 1));
+                    markedCard[i][j] = true;
+                    prettyPrintMarkedCard();
+                    return true;
+                }
+            }
+        }
+        System.out.println("called number: " + calledNumber + " was not present on card " + this.name);
+        return false;
     }
 
     void initInitialBingoCard(int... numbers) {
@@ -28,11 +101,25 @@ public class BingoCard {
         }
     }
 
-    public void prettyPrintCard() {
-        System.out.println("Pretty print for card: " + name);
+    public void prettyPrintInitialCard() {
+        System.out.println("---------------------");
+        System.out.println("Pretty print initalCard for card: " + name);
+        System.out.println("---------------------");
         for(int i = 0; i < 5; i++) { //every 5 items
             for (int j = 0; j < 5; j++) { //column
                 System.out.print(initalBingoCard[i][j] + " "); // print for column
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public void prettyPrintMarkedCard() {
+        System.out.println("---------------------");
+        System.out.println("Pretty print markedCard for card: " + name);
+        System.out.println("---------------------");
+        for(int i = 0; i < 5; i++) { //every 5 items
+            for (int j = 0; j < 5; j++) { //column
+                System.out.print(markedCard[i][j] + " "); // print for column
             }
             System.out.print("\n");
         }
--- src/main/java/com/wytzig/lw/nl/advent/twentytwentyone/bingo/Day4.java
+++ src/main/java/com/wytzig/lw/nl/advent/twentytwentyone/bingo/Day4.java
@@ -17,38 +17,59 @@ public class Day4 implements Day {
             Arrays.asList(7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1)
     );
 
-    public void initBingoCardsFromExample() {
-        System.out.println("---- Initializing bingo cards for example ----");
-        BingoCard card1 = new BingoCard("card1");
-        card1.initInitialBingoCard(22, 13, 17, 11,  0, 8, 2, 23,  4, 24, 21, 9, 14, 16,  7, 6, 10, 3, 18, 5, 1, 12, 20, 15, 19);
-        System.out.println("printing card 1: ");
-        card1.prettyPrintCard();
-        BingoCard card2 = new BingoCard("card2");
-        card2.initInitialBingoCard(3, 15,  0,  2, 22,
-                9, 18, 13, 17,  5,
-                19,  8,  7, 25, 23,
-                20, 11, 10, 24,  4,
-                14, 21, 16, 12,  6);
-        card2.prettyPrintCard();
-        BingoCard card3 = new BingoCard("card3");
-        card3.initInitialBingoCard(14, 21, 17, 24,  4,
-                10, 16, 15, 9, 19,
-                18,  8, 23, 26, 20,
-                22, 11, 13, 6, 5,
-                2 , 0, 12, 3, 7);
-        card3.prettyPrintCard();
-        System.out.println("---- Finished initializing ----");
-    }
+    private BingoCard card1;
+    private BingoCard card2;
+    private BingoCard card3;
 
     @Override
     public boolean getAnswerFromExample() {
         initBingoCardsFromExample();
-        return false;
+        for(int number: pulledBingoBallsInOrder) {
+            if(card1.callNumberAndCheckForBingo(number) ||
+                    card2.callNumberAndCheckForBingo(number) ||
+                    card3.callNumberAndCheckForBingo(number)) {
+                // we have bingo so exit
+                return true;
+            }
+        }
+        return false; // no bingo :(
     }
 
     @Override
     public boolean getAnswerForQuestion() {
         return false;
+    }
+
+    public void initBingoCardsFromExample() {
+        System.out.println("---- Initializing bingo cards for example ----");
+        card1 = new BingoCard("card1");
+        card1.initInitialBingoCard(
+                22, 13, 17, 11, 0,
+                8, 2, 23,  4, 24,
+                21, 9, 14, 16,  7,
+                6, 10, 3, 18, 5, 1,
+                12, 20, 15, 19);
+        System.out.println("printing card 1: ");
+        card1.prettyPrintInitialCard();
+
+        card2 = new BingoCard("card2");
+        card2.initInitialBingoCard(
+                3, 15,  0,  2, 22,
+                9, 18, 13, 17,  5,
+                19,  8,  7, 25, 23,
+                20, 11, 10, 24,  4,
+                14, 21, 16, 12,  6);
+        card2.prettyPrintInitialCard();
+
+        card3 = new BingoCard("card3");
+        card3.initInitialBingoCard(
+                14, 21, 17, 24,  4,
+                10, 16, 15, 9, 19,
+                18,  8, 23, 26, 20,
+                22, 11, 13, 6, 5,
+                2 , 0, 12, 3, 7);
+        card3.prettyPrintInitialCard();
+        System.out.println("---- Finished initializing ----");
     }
 
 }
