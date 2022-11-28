79207fc2e9331889d3415dd04901578e1cace7be
wytzig <luukweiss@gmail.com>
2022-11-28 21:35:34 +0100

    completed algorithm for example, great success

--- src/main/java/com/wytzig/lw/nl/advent/twentytwentyone/magmaheatmap/DayNine.java
+++ src/main/java/com/wytzig/lw/nl/advent/twentytwentyone/magmaheatmap/DayNine.java
@@ -4,6 +4,7 @@ import com.wytzig.lw.nl.advent.Day;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,6 +12,8 @@ public class DayNine implements Day {
 
     private List<String> heatmapRows = new ArrayList<>();
     private long[][] heatmap;
+
+    private List<Long> listOfLowPoints = new ArrayList<>();
 
     /**
      * Converts input file to rows in @param heatmapRows
@@ -43,12 +46,77 @@ public class DayNine implements Day {
         }
     }
 
+    private long getLocation(Positions positions, int i, int j) {
+        long result = -1; // ASSUMPTION input will not have any negative numbers || future solve with Optional.EMPTY()
+        try {
+            switch (positions) {
+                case LEFT -> {
+                    result = heatmap[i][j - 1];
+                }
+                case RIGHT -> {
+                    result = heatmap[i] [j + 1];
+                }
+                case UP -> {
+                    result = heatmap[i - 1][j];
+                }
+                case DOWN -> {
+                    result = heatmap[i + 1][j];
+                }
+            }
+        } catch (Exception e) {
+//            System.out.println("We skip " + positions);
+        } finally {
+            return result;
+        }
+    }
+
+    private boolean isLower(long location, long compareTo) {
+        if(compareTo != -1) {
+            return location < compareTo;
+        }
+        return true; // no value means out of bounds, safe to assume location will be low
+    }
+
+    private void determineLowPoints() {
+        for(int i = 0; i < this.heatmap.length; i++) { // for all columns
+            assert this.heatmap.length == this.heatmapRows.size();
+            assert Arrays.stream(this.heatmap).findAny().get().length == this.heatmapRows.stream().findAny().get().length();
+            for(int j = 0; j < Arrays.stream(this.heatmap).findAny().get().length; j++) { // for every element in the row
+                long location = this.heatmap[i][j];
+
+                long left = getLocation(Positions.LEFT, i, j);
+                long right = getLocation(Positions.RIGHT, i, j);
+                long upper = getLocation(Positions.UP, i, j);
+                long under = getLocation(Positions.DOWN, i, j);
+
+                if(isLower(location, left) && isLower(location, right) && isLower(location, upper) && isLower(location, under)) {
+                    System.out.println("low point detected: " + location);
+                    this.listOfLowPoints.add(location);
+                }
+
+                System.out.println("the current number is: " + location + ". left: " + left + ". right: " + right + ". up: " + upper + ". under: "+ under);
+            }
+            System.out.println("\n");
+        }
+    }
+
+    public long caluculateRiskFromLowPoints() {
+        long sum = 0;
+        for(long location: listOfLowPoints) {
+            sum += (location + 1);
+        }
+        System.out.println("The sum of all risk points is: " + sum);
+        return sum;
+    }
+
 
     @Override
     public boolean getAnswerFromExample() {
         initInputForInputFile("C:\\Users\\Admin\\projects\\advent\\src\\main\\java\\com\\wytzig\\lw\\nl\\advent\\twentytwentyone\\input_files\\day9_example_input.txt");
         processHeatMapRows();
-        return false;
+        determineLowPoints();
+        caluculateRiskFromLowPoints();
+        return true;
     }
 
     @Override
---
+++ src/main/java/com/wytzig/lw/nl/advent/twentytwentyone/magmaheatmap/Positions.java
@@ -0,0 +1,8 @@
+package com.wytzig.lw.nl.advent.twentytwentyone.magmaheatmap;
+
+public enum Positions {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+}
