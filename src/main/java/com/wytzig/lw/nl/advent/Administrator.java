8180f7915d55b255b5c6c29891c78435d78b94e4
wytzig <luukweiss@gmail.com>
2022-12-26 01:39:06 +0100

    updated docs

--- src/main/java/com/wytzig/lw/nl/advent/Administrator.java
+++ src/main/java/com/wytzig/lw/nl/advent/Administrator.java
@@ -5,7 +5,17 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Stack;
 
+/**
+ * Administrator abstract class which provides a universal execution of initialized Day objects;
+ *  1. Initialize your year days in your administrator implementation.
+ *  2. Call the execution function from your application.
+ *
+ * possible improvements:
+ *  - write to output files instead of console
+ */
 public abstract class Administrator {
+
+    /* NOTE: Don't forget to add the default constructor of your class and initialize the init method! */
 
     protected List<Day> days = new Stack<>();
 
--- src/main/java/com/wytzig/lw/nl/advent/twentytwentytwo/AdministratorTwentyTwentyTwo.java
+++ src/main/java/com/wytzig/lw/nl/advent/twentytwentytwo/AdministratorTwentyTwentyTwo.java
@@ -1,7 +1,6 @@
 package com.wytzig.lw.nl.advent.twentytwentytwo;
 
 import com.wytzig.lw.nl.advent.Administrator;
-import com.wytzig.lw.nl.advent.Day;
 import com.wytzig.lw.nl.advent.twentytwentytwo.campcleaning.DayFour;
 import com.wytzig.lw.nl.advent.twentytwentytwo.communicationdevice.DaySix;
 import com.wytzig.lw.nl.advent.twentytwentytwo.elvescalories.DayOne;
@@ -9,16 +8,6 @@ import com.wytzig.lw.nl.advent.twentytwentytwo.rockpaperscissors.DayTwo;
 import com.wytzig.lw.nl.advent.twentytwentytwo.rucksackCheck.DayThree;
 import com.wytzig.lw.nl.advent.twentytwentytwo.stackingsnacks.DayFive;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.List;
-import java.util.Stack;
-
-/**
- * possible improvements:
- *  - write to output files instead of console
- *  -
- */
 public class AdministratorTwentyTwentyTwo extends Administrator {
     protected void init() {
         this.days.add(new DayOne());
